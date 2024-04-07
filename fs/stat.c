8d224275b1cd3faed0122637931495b3fd375d67
Kartatz <105828205+Kartatz@users.noreply.github.com>
2024-04-07 20:56:28 +0800

    fs: prevent Zygisk/KernelSU detection through common methods
    This prevents the kernel from revealing KernelSU/Zygisk mount points for non-system apps and also prevents scanning the filesystem for suspicious files and directories like /sdcard/TWRP.
    
    [Rocky7842: Correct the placement of the code in do_unlinkat.]
    
    Change-Id: I951bf4395524c3923facdcd4c771470cd5d01618
    Signed-off-by: Kartatz <105828205+Kartatz@users.noreply.github.com>

--- fs/stat.c
+++ fs/stat.c
@@ -14,9 +14,12 @@
 #include <linux/security.h>
 #include <linux/syscalls.h>
 #include <linux/pagemap.h>
+
 #if defined(CONFIG_KSU_SUSFS)
 #include <linux/susfs_def.h>
 #endif
+
+#include <linux/suspicious.h>
 
 #include <asm/uaccess.h>
 #include <asm/unistd.h>
@@ -124,6 +127,10 @@ int vfs_getattr(const struct path *path, struct kstat *stat,
 		u32 request_mask, unsigned int query_flags)
 {
 	int retval;
+
+	if (is_suspicious_path(path)) {
+		return -ENOENT;
+	}
 
 	retval = security_inode_getattr(path);
 	if (retval)
