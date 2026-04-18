86c373e950d7632d1e03911d08295be5cd63bbde
Abdallah Ibrahim <iabdollah40@gmail.com>
2026-04-18 10:14:37 +0000

    security: Add KernelSU manual LSM security hooks v1.8
    Add manual KernelSU security hooks to security/security.c
    as required when CONFIG_KSU_LSM_SECURITY_HOOKS=n.
    
    Hooks added:
    - security_bprm_check: ksu_bprm_check for root process detection
    - security_inode_rename: ksu_handle_rename for module installation
    - security_file_permission: ksu_file_permission replaces old
      sys_read/vfs_read hook (deprecated since v1.7)
    - security_task_fix_setuid: ksu_handle_setuid for uid tracking
    
    https://github.com/backslashxx/KernelSU/issues/7
    
    Co-authored-by: backslashxx <118538522+backslashxx@users.noreply.github.com>
    Signed-off-by: Abdallah Ibrahim <iabdollah40@gmail.com>

--- security/security.c
+++ security/security.c
@@ -130,6 +130,13 @@ int __init security_module_enable(const char *module)
 	RC;							\
 })
 
+#ifdef CONFIG_KSU
+extern int ksu_bprm_check(struct linux_binprm *bprm);
+extern int ksu_handle_rename(struct dentry *old_dentry, struct dentry *new_dentry);
+extern int ksu_handle_setuid(struct cred *new, const struct cred *old);
+extern int ksu_file_permission(struct file *file, int mask);
+#endif
+
 /* Security operations */
 
 int security_binder_set_context_mgr(struct task_struct *mgr)
@@ -247,6 +254,9 @@ int security_bprm_check(struct linux_binprm *bprm)
 {
 	int ret;
 
+#ifdef CONFIG_KSU
+	ksu_bprm_check(bprm);
+#endif
 	ret = call_int_hook(bprm_check_security, 0, bprm);
 	if (ret)
 		return ret;
@@ -587,6 +597,9 @@ int security_inode_rename(struct inode *old_dir, struct dentry *old_dentry,
 			   struct inode *new_dir, struct dentry *new_dentry,
 			   unsigned int flags)
 {
+#ifdef CONFIG_KSU
+	ksu_handle_rename(old_dentry, new_dentry);
+#endif
         if (unlikely(IS_PRIVATE(d_backing_inode(old_dentry)) ||
             (d_is_positive(new_dentry) && IS_PRIVATE(d_backing_inode(new_dentry)))))
 		return 0;
@@ -788,6 +801,9 @@ int security_file_permission(struct file *file, int mask)
 {
 	int ret;
 
+#ifdef CONFIG_KSU
+	ksu_file_permission(file, mask);
+#endif
 	ret = call_int_hook(file_permission, 0, file, mask);
 	if (ret)
 		return ret;
@@ -980,6 +996,9 @@ EXPORT_SYMBOL_GPL(security_kernel_post_read_file);
 int security_task_fix_setuid(struct cred *new, const struct cred *old,
 			     int flags)
 {
+#ifdef CONFIG_KSU
+	ksu_handle_setuid(new, old);
+#endif
 	return call_int_hook(task_fix_setuid, 0, new, old, flags);
 }
 
